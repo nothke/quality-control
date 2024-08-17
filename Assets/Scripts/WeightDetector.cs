f20994a8dbcadc8404e4b9e0263f1d7d501388bf
nothke <nothke@gmail.com>
2024-08-17 11:55:20 +0200

    Weight detector

---
+++ Assets/Scripts/WeightDetector.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeightDetector : MonoBehaviour
+{
+    HashSet<Rigidbody> enteredBodies = new HashSet<Rigidbody>();
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var rb = collision.rigidbody;
+
+        if (rb)
+        {
+            enteredBodies.Add(rb);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        var rb = collision.rigidbody;
+
+        if (rb)
+        {
+            enteredBodies.Remove(rb);
+        }
+    }
+
+    public float GetTotalWeight()
+    {
+        float totalMass = 0;
+        foreach (var body in enteredBodies)
+        {
+            totalMass += body.mass;
+        }
+
+        return totalMass;
+    }
+
+    private void Update()
+    {
+        Debug.Log(GetTotalWeight());
+    }
+}
