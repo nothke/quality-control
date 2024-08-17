874095249394ff9d64203907639150d30fef9514
Khauvinkh <khauvinkh@gmail.com>
2024-08-17 13:59:08 +0200

    Basic staging utility

---
+++ Assets/Scripts/Game Schedule/StageProp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class StageProp: MonoBehaviour
+{
+    public StagingManager.StageEnum ActiveAtStages;
+
+    public void Start()
+    {
+        StagingManager.RegisterStageProp(this);
+        gameObject.SetActive(false);
+    }
+    
+    public void OnDestroy()
+    {
+        StagingManager.RemoveStageProp(this);
+    }
+}
---
+++ Assets/Scripts/Game Schedule/StagingManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class StagingManager: MonoBehaviour
+{
+    public static StagingManager Instance;
+    public static List<StageProp> StageProps;
+
+    public StageEnum CurrentStage;
+    
+    [Flags]
+    public enum StageEnum
+    {
+        Level1 = 1,
+        Level2 = 2,
+    }
+
+    public void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    public void OnDestroy()
+    {
+        StageProps.Clear();
+    }
+
+    public static void RegisterStageProp(StageProp stageProp)
+    {
+        if (StageProps == null)
+        {
+            StageProps = new List<StageProp>();
+        }
+
+        if (!StageProps.Contains(stageProp))
+        {
+            StageProps.Add(stageProp);
+        }
+    }
+    
+    public static void RemoveStageProp(StageProp stageProp)
+    {
+        if (StageProps.Contains(stageProp))
+        {
+            StageProps.Remove(stageProp);
+        }
+    }
+    
+    public static void SetStage(StageEnum stage)
+    {
+        Instance.CurrentStage = stage;
+        
+        foreach (var stageProp in StageProps)
+        {
+            stageProp.gameObject.SetActive((stageProp.ActiveAtStages & stage) != 0);
+        }
+    }
+
+    [MenuItem("Tools/Staging/Level1")]
+    public static void SetStage1()
+    {
+        SetStage(StageEnum.Level1);
+    }
+    
+    [MenuItem("Tools/Staging/Level2")]
+    public static void SetStage2()
+    {
+        SetStage(StageEnum.Level2);
+    }
+}
